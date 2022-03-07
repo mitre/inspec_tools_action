0ede8c7df529848b47e9a512533b4488a89b821d
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-03-07 15:49:55 -0600

    test changing environment variable name

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,17 @@ set -e
 
 echo $PATH
 echo $INPUT_THRESHOLDFILE
-echo $INPUT_RESULTFILE
+echo $INPUT_RESULTSFILE
 echo $INPUT_COMMAND
 
-if [ -n $INPUT_THRESHOLDFILE ] && [ -n $INPUT_COMMAND ] && [ -n $INPUT_RESULTFILE ]; then
+if [ -n $INPUT_THRESHOLDFILE ] && [ -n $INPUT_COMMAND ] && [ -n $INPUT_RESULTSFILE ]; then
   echo "Using $(inspec_tools version)"
   case "$INPUT_COMMAND" in
     "compliance" )
-      inspec_tools compliance -j "$INPUT_RESULTFILE" -f "$INPUT_THRESHOLDFILE"
+      inspec_tools compliance -j "$INPUT_RESULTSFILE" -f "$INPUT_THRESHOLDFILE"
       ;;
     "summary" )
-      inspec_tools summary -j "$INPUT_RESULTFILE" -t "$INPUT_THRESHOLDFILE"
+      inspec_tools summary -j "$INPUT_RESULTSFILE" -t "$INPUT_THRESHOLDFILE"
       ;;
     * )
       echo "$INPUT_COMMAND is not a valid command";
